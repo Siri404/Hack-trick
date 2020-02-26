f1f13e6c3da77a16cec9ef74bdd2423991ac0cb6
Siri404 <esiritanu@gmail.com>
2020-02-26 15:26:50 +0200

    Version 1.01

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public TMP_Text text;
+    public GameSystem gameSystem;
+    public void GameOverDialogue()
+    {
+        if (gameSystem.state == GameState.Won)
+        {
+            text.text = "You Won!";
+        }
+        else if (gameSystem.state == GameState.Lost)
+        {
+            text.text = "You Lost!";
+        }
+        else return;
+        
+        gameObject.SetActive(true);
+    }
+}
