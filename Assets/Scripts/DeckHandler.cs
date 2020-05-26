720097f28de1bd8633807de521f891140bf5a5f1
Siri404 <esiritanu@gmail.com>
2020-05-26 20:08:02 +0300

    refactor
    sound rename + unused imports removed from deckHandler

--- Assets/Scripts/DeckHandler.cs
+++ Assets/Scripts/DeckHandler.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 using Random = System.Random;
 
 public class DeckHandler : MonoBehaviour
